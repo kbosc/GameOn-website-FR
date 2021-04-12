0984315e1c538e7ddd0f4746f21517c1feb1d57c
Kevin Bosc <kevin_bosc@hotmail.fr>
2021-04-13 00:06:41 +0200

    modal closed with X

--- starterOnly/script.js
+++ starterOnly/script.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+document.querySelector(".close").addEventListener("click", onClick);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,4 +19,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal
+function onClick(e) {
+  console.log("click");
+  modalbg.style.display = "";
 }
