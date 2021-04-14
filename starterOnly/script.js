c2c9868d7f86e8b8e71e138f3620599e1c49c64e
Kevin Bosc <kevin_bosc@hotmail.fr>
2021-04-14 23:47:22 +0200

    validate radio button checked

--- starterOnly/script.js
+++ starterOnly/script.js
@@ -12,6 +12,13 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 document.querySelector(".close").addEventListener("click", onClick);
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const mail = document.getElementById("email");
+const numberTournament = document.getElementById("quantity");
+const radios = document.getElementsByName("location");
+const formSubmit = document.getElementById("submit");
+const checkAgree = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,4 +32,27 @@ function launchModal() {
 function onClick(e) {
   console.log("click");
   modalbg.style.display = "";
+}
+
+// Validate Mail
+// function ValidateMail {
+
+// }
+
+// Terms of use checked
+function agreeAccepted() {
+  if (!checkAgree.checked) {
+    alert("Must check some option!");
+  }
+}
+// Validation Radio Button
+function validateForm() {
+  let formValid = false;
+  let i = 0;
+  while (!formValid && i < radios.length) {
+    if (radios[i].checked) formValid = true;
+    i++;
+  }
+  if (!formValid) alert("Must check some option!");
+  return formValid;
 }
