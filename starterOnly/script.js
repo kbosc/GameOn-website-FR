99582a97e4c786a5aba36d64b1f3541fd82a6977
Kevin Bosc <kevin_bosc@hotmail.fr>
2021-04-27 22:04:51 +0200

    delete comment useless

--- starterOnly/script.js
+++ starterOnly/script.js
@@ -7,6 +7,26 @@ function editNav() {
   }
 }
 
+/* When the user clicks on the dropdown,
+toggle between hiding and showing content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
